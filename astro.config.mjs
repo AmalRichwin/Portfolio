f2aec3e93d0bbdd1a5ec0c77d51f0fd5daf7afcb
AmalRichwin <amalrichwin800@gmail.com>
2024-01-27 01:13:12 +0530

    add cloudflare plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
+import cloudflare from "@astrojs/cloudflare";
 const SERVER_PORT = 3001;
 // the url to access your blog during local development
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 // the url to access your blog after deploying it somewhere (Eg. Netlify)
-const LIVE_URL = "https://yourwebsiteurl.com";
+const LIVE_URL = "https://richwin.in";
 // this is the astro command your npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
@@ -24,5 +25,7 @@ export default defineConfig({
   server: {
     port: SERVER_PORT
   },
-  integrations: [tailwind(), sitemap(), react(), markdoc()]
+  integrations: [tailwind(), sitemap(), react(), markdoc()],
+  output: "server",
+  adapter: cloudflare()
 });
